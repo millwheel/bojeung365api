3a025f9147842fe9e1c0dd6c9c3480d7c2afeb3e
sijun <sionwer5@gmail.com>
2025-10-03 16:21:25 +0900

    feat: rest authentciation failuer handler 추가

---
+++ src/main/java/com/example/bojeung365api/security/handler/RestAuthenticationFailureHandler.java
@@ -0,0 +1,23 @@
+package com.example.bojeung365api.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class RestAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), exception.getMessage());
+    }
+}
