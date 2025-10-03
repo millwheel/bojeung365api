e78f0653b0fc1515fb6fef59f3090f20a798f4ab
sijun <sionwer5@gmail.com>
2025-10-03 22:16:03 +0900

    feat: comment 엔티티 추가

---
+++ src/main/java/com/example/bojeung365api/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.bojeung365api.entity;
+
+import com.example.bojeung365api.entity.post.Post;
+import com.example.bojeung365api.entity.user.User;
+import lombok.Getter;
+import jakarta.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String body;
+
+}
