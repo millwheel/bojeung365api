dd2325ed9a39505da79dab607cbe23a302bb0c75
sijun <sionwer5@gmail.com>
2025-10-07 20:41:13 +0900

    feat: 테스트용 db는 h2로 설정

--- src/test/java/com/example/bojeung365api/Bojeung365apiApplicationTests.java
+++ src/test/java/com/example/bojeung365api/Bojeung365apiApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.bojeung365api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Bojeung365apiApplicationTests {
 
     @Test
