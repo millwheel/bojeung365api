c9b1aaefd84b9604d49e8e1dcf7eacc0446a3aa8
sijun <sionwer5@gmail.com>
2025-10-14 21:43:11 +0900

    feat: user entity의 id 전략을 identity로 변경

--- src/main/java/com/example/bojeung365api/entity/user/User.java
+++ src/main/java/com/example/bojeung365api/entity/user/User.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userId")
     private Long id;
 
