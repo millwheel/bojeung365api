a1b630394d5d40bfed402ab701a5046ac8ad6fdf
sijun <sionwer5@gmail.com>
2025-10-18 19:32:01 +0900

    feat: aws config 추가

---
+++ src/main/java/com/example/bojeung365api/config/AwsConfig.java
@@ -0,0 +1,24 @@
+package com.example.bojeung365api.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Slf4j
+@Configuration
+public class AwsConfig {
+
+    private String bucket;
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
