3b5e8c0e2bc169522d4b5322dc96a310d966fa38
sijun <sionwer5@gmail.com>
2025-10-19 17:01:21 +0900

    feat: aws config 설정 코드 추가

--- src/main/java/com/example/bojeung365api/config/AwsConfig.java
+++ src/main/java/com/example/bojeung365api/config/AwsConfig.java
@@ -1,6 +1,7 @@
 package com.example.bojeung365api.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
@@ -11,7 +12,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class AwsConfig {
 
-    private String bucket;
+    @Value("${aws.region}")
     private String region;
 
     @Bean
