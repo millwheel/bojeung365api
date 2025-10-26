35f919d9d0a1fc5eaabd8931263625fab72825b5
sijun <sionwer5@gmail.com>
2025-10-26 20:09:25 +0900

    feat: cookie same site 설정 변경

--- src/main/java/com/example/bojeung365api/security/config/SecurityConfig.java
+++ src/main/java/com/example/bojeung365api/security/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.context.DelegatingSecurityContextReposit
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -113,6 +114,11 @@ public class SecurityConfig {
         DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
         handler.setRoleHierarchy(roleHierarchy);
         return handler;
+    }
+
+    @Bean
+    public CookieSameSiteSupplier cookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
     }
 
 }
